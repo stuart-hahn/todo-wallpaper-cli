e2d7e681bbfd137c2a7e8b5e69eee85a6511f1b1
stuart-hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-09-29 16:58:59 -0400

    feat: add 'list' command to list all tasks

--- index.js
+++ index.js
@@ -13,11 +13,24 @@ const addTask = (task) => {
   console.log(`Task added: ${task}`);
 };
 
+const listTasks = (tasks) => {
+  if (tasks.length === 0) {
+    console.log("No tasks to show. Add a task using the 'add' command.");
+    return;
+  }
+
+  tasks.forEach((task, index) => {
+    console.log(`${index + 1}. ${task}`);
+  });
+};
+
 const inputCommand = process.argv[2];
 const inputTask = process.argv.slice(3).join(" ");
 
 if (inputCommand === "add") {
   addTask(inputTask);
+} else if (inputCommand === "list") {
+  listTasks(tasks);
 } else {
   console.log("Unknown command. Please use 'add' to add a task.");
 }
